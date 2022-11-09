7f4618c55273a0c301bdb191fcda634300a90723
Oleksandr Vasianovych <derdef360@gmail.com>
2022-11-09 22:12:46 +0200

    editing

--- SanaPrakt02/Task1/Task1.cpp
+++ SanaPrakt02/Task1/Task1.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Курси Sana Commerce Software Development Bootcamp\n";
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
